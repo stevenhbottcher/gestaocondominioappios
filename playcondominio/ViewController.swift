7f89c1958a30248ca27faa45adcbcc998e66a60a
Paulo Achilles Morato de Carvalho <pauloachillesmoratodecarvalho@macbook-air-de-paulo.local>
2019-06-21 16:30:40 -0300

    Initial Commit

---
+++ playcondominio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playcondominio
+//
+//  Created by Paulo Achilles Morato de Carvalho on 21/06/19.
+//  Copyright © 2019 AMPM Consultoria em Informática. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
